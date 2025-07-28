ad9c6e4628970d63153935f914d12a47f39af1ab
alinakassym <alinakassym@gmail.com>
2025-07-28 14:38:31 +0500

    feat: replace mock data with fetch from json-server

--- src/App.jsx
+++ src/App.jsx
@@ -2,35 +2,13 @@ import { useState } from "react";
 import styles from "./App.module.css";
 import { useEffect } from "react";
 
-const MOCK_DATA = [
-  {
-    id: "001",
-    name: "4K Smart TV Samsung 55",
-    price: 749,
-  },
-  {
-    id: "002",
-    name: "iPhone 16 Pro Max 1024GB",
-    price: 999,
-  },
-  {
-    id: "003",
-    name: "Dyson Supersonic Hair Dryer",
-    price: 429,
-  },
-];
-
 function App() {
   const [products, setProducts] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     setIsLoading(true);
-    new Promise((resolve) => {
-      setTimeout(() => {
-        resolve({ json: () => MOCK_DATA });
-      }, 2000);
-    })
+    fetch("http://localhost:3000/products")
       .then((responce) => responce.json())
       .then((data) => {
         setProducts(data);
