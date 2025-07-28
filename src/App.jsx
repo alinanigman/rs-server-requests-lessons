41fe7dec88c1434d4498b45d80dd69e33f3c8914
alinakassym <alinakassym@gmail.com>
2025-07-28 15:58:37 +0500

    feat: add handleAddProduct

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,25 @@ function App() {
   const [products, setProducts] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
+  const handleAddProduct = () => {
+    console.log("Adding new product...");
+    const newProduct = {
+      name: "New Product",
+      price: Math.floor(Math.random() * 100) + 1,
+    };
+
+    fetch("http://localhost:3000/products", {
+      method: "POST",
+      headers: { "Content-Type": "application/json;charset=UTF-8" },
+      body: JSON.stringify(newProduct),
+    })
+      .then((res) => res.json())
+      .then((createdProduct) => {
+        console.log("Created product:", createdProduct);
+        setProducts([...products, createdProduct]);
+      });
+  };
+
   useEffect(() => {
     setIsLoading(true);
     fetch("http://localhost:3000/products")
@@ -21,19 +40,19 @@ function App() {
     <div className={styles.App}>
       <div className={styles.header}>
         <h1>Products</h1>
-        <button>Add new</button>
+        <button onClick={handleAddProduct}>Add new</button>
       </div>
       <div className={styles.list}>
         {isLoading ? (
           <div className={styles.loader}></div>
         ) : (
           products.map((product) => (
-            <>
-              <div key={product.id} className={styles.product}>
+            <div className={styles.item} key={product.id}>
+              <div className={styles.product}>
                 <b>{product.name}</b>
               </div>
               <div>${product.price}</div>
-            </>
+            </div>
           ))
         )}
       </div>
